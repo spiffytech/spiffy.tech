84b7a14a0c50ecb1f4a54309489d9d2535662819
spiffytech <spiffy@spiffy.tech>
2019-08-14 13:02:32 -0400

    Try using hljs for syntax highlighting

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -14,11 +14,19 @@
 
 <script>
   import fm from 'front-matter';
+  import hljs from 'highlight.js';
   import MarkdownIt from 'markdown-it';
+  import {afterUpdate} from 'svelte';
 
   import Newsletter from '../../components/Newsletter.svelte';
 
   export let postMd;
+
+	afterUpdate(() => {
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+	});
 
   const md = new MarkdownIt();
 
@@ -30,46 +38,22 @@
 </script>
 
 <style>
-  /*
-      By default, CSS is locally scoped to the component,
-      and any unused styles are dead-code-eliminated.
-      In this page, Svelte can't know which elements are
-      going to appear inside the {{{post.html}}} block,
-      so we have to use the :global(...) modifier to target
-      all elements inside .content
-  */
-  .content :global(h2) {
-    font-size: 1.4em;
-    font-weight: 500;
-  }
+	.content :global(h1) {
+	font-weight: 700;
+	font-size: 1.5rem;
+}
 
-  .content :global(pre) {
-    background-color: #f9f9f9;
-    box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
-    padding: 0.5em;
-    border-radius: 2px;
-    overflow-x: auto;
-  }
-
-  .content :global(pre) :global(code) {
-    background-color: transparent;
-    padding: 0;
-  }
-
-  .content :global(ul) {
-    line-height: 1.5;
-  }
-
-  .content :global(li) {
-    margin: 0 0 0.5em 0;
-  }
+.content :global(*) {
+	margin-bottom: 1.25rem;
+}
 </style>
 
 <svelte:head>
   <title>{post.title}</title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/styles/github.min.css">
 </svelte:head>
 
-<h1>{post.title}</h1>
+<h1 class="font-bold text-3xl mb-5">{post.title}</h1>
 
 <div class="content">
   {@html post.html}
