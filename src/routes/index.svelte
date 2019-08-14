466b8486c137bbade0042f8abe08d3084282b7db
spiffytech <spiffy@spiffy.tech>
2019-08-13 21:44:51 -0400

    More Netlify Identity

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -46,6 +46,7 @@
 
 <svelte:head>
 	<title>Sapper project template</title>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <h1>Great success!</h1>
