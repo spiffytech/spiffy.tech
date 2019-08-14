909c759e89d33579200337a1096a73483fb19eb4
spiffytech <spiffy@spiffy.tech>
2019-08-14 03:13:48 -0400

    Fixed site index to load correctly on SSR

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
+  import {onMount} from 'svelte';
+
+  onMount(() => {
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
         });
       }
-    });
-  }
+  });
 </script>
 
 <style>
