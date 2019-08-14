c7ac2aef1947d2f926b0ea1b078992206434b949
spiffytech <spiffy@spiffy.tech>
2019-08-14 17:19:30 -0400

    Increase the contrast for blog post cards

--- src/components/Posts.svelte
+++ src/components/Posts.svelte
@@ -6,7 +6,7 @@
   <a rel="prefetch" href={`blog/${post.slug}`}>
     <article
       class="p-5 mb-5 shadow rounded-lg bg-cover"
-      style={`background: linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("${post.thumbnail}")`}>
+      style={`background: linear-gradient(rgba(255,255,255,.85), rgba(255,255,255,.85)), url("${post.thumbnail}")`}>
       <heading class="font-bold text-2xl">{post.title}</heading>
       <p>{post.excerpt}</p>
     </article>
