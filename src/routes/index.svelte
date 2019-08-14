e5d2e7afc6cb2814353e85c185ba0568380e181c
spiffytech <spiffy@spiffy.tech>
2019-08-14 11:54:56 -0400

    Tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,15 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
 <script>
   import {onMount} from 'svelte';
+
+  export let posts;
 
   onMount(() => {
       if (window.netlifyIdentity) {
@@ -14,50 +24,14 @@
   });
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>Sapper project template</title>
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+{#each posts as post}
+  <article>
+    <a href={`blog/${post.slug}`}><heading>{post.title}</heading></a>
+    <p>{post.excerpt}</p>
+  </article>
+{/each}
