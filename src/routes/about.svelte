147319962486bcb144076f78b5234f4ca1f31975
spiffytech <spiffy@spiffy.tech>
2019-08-15 10:50:04 -0400

    Fixed styling on the about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,6 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About spiffytech</h1>
+<h1 class="text-3xl font-bold">About spiffytech</h1>
 
 <p>My name is Brian Cottingham, a.k.a. spiffytech. I'm a software developer in North Carolina, and I love working on small projects using cool tools. I specialize in JavaScript work, and lately have been using a lot of Svelte.</p>
